642db0c3ae8883abfbf48f4bf3cb8130da686712
Алексей Немытых <alexnemimikh@gmail.com>
2024-04-24 01:57:03 +0500

    настроена миграция

--- autoservis/database/base_meta.py
+++ autoservis/database/base_meta.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from config import DB_HOST, DB_PORT, DB_PASSWORD, DB_USER, DB_NAME
 
 Base = declarative_base()
-print(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 # engine = create_engine("sqlite:///../AutoServis.db", echo=False)
 engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=False)
 get_session = sessionmaker(engine, autocommit=False)
